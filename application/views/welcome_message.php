7112a4d063b414d3c6ac3e39020b20e125b43096
703PVRTCX <kasaokapapkies@gmail.com>
2021-06-22 02:01:43 +0200

    Incomplete Website replicate

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,115 +1,88 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html lang="en">
 <head>
 	 <!-- Required meta tags -->
 	 <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-	<title>Welcome to Teklink</title>
+	<title>Teklink</title>
 
-
-	<style type="text/css">
-
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
-
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-	}
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
-
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
-
-	#body {
-		margin: 0 15px 0 15px;
-	}
-
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
 </head>
 <body>
+<div class="banner">
+	<div class ="navbar"> 
+		
+	<img class="logo_image" alt="logo_image"
+		<?php 
+				echo "<img style='height:150px'; src ='assets/img/logo_image.png'>";	
+		?>
+	>
+		<ul >
+			<li><a href="#home">Home</a></li>
+			<li><a href="#about">About us</a></li>
+			<li><a href="#service">services</a></li>
+			<li><a href="#approach">Approach</a></li>
+			<li><a href="#portfolio">Portfolio</a></li>
+			<li><a href="#contact">Contact</a></li>
+			<hr>
+		</ul>
+	</div>
+	<div class ="content">
+		<h1>End-to-End Product Engineering</h1>
+		<p>We deliver high value innovated solutions by bridging the gap between business and technology.
+			<br>
+		</p>
+		<div>
+			<button type="button"><span></span>READ MORE!</button>
+			
+			<button type="button"><span></span>CONTACT US</button>
+		</div>
 
-<ul class="nav justify-content-end">
-  <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#">Active</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-  </li>
-</ul>
-
-
-<div id="container">
-	<h1>Welcome to Teklink</h1>
-
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+	</div>
+</div>
+<div class ="about" id="about">
+		<h1>About Us</h1>
+		<p>Loren ipsom
+			<br>
+		</p>
+	</div>
+	<div class ="service" id="service">
+		<h1>Services</h1>
+		<p>Loren ipsom
+			<br>
+		</p>
+	</div>
+	<div class ="approach" id="approach">
+		<h1>Approach</h1>
+		<p>Loren ipsom
+			<br>
+		</p>
+		
+	</div>
+	<div class ="portfolio" id="portfolio">
+		<h1>Portfolio</h1>
+		<p>Loren ipsom
+			<br>
+		</p>
+		
+	</div>
+	<div class ="contact" id="contact">
+		<h1>Contact Us</h1>
+		<p>Loren ipsom
+			<br>
+		</p>
 	</div>
 
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
-</div>
+
  <!-- Option 1: Bootstrap Bundle with Popper -->
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
   <!-- Option 2: Separate Popper and Bootstrap JS -->
 <!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script> -->
-
-
 </body>
 </html>
